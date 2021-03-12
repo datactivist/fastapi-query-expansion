189038a6b1957d64c08d43f493919f7477ebda32
AnthonyGigerich <anthony@datactivist.coop>
2021-03-12 15:39:30 +0100

    fix

--- create_docker.sh
+++ create_docker.sh
@@ -8,5 +8,5 @@ echo "Downloading done!"
 echo ""
 
 sudo docker build -t fastapi-query-expansion:1.0.0 .
-sudo docker rm apitest
-sudo docker run -p 80:80 --name fastapi-query-expansion:1.0.0  fastapi-query-expansion:1.0.0
+sudo docker rm fastapi-query-expansion
+sudo docker run -p 80:80 --name fastapi-query-expansion fastapi-query-expansion:1.0.0
